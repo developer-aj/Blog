e6a2ed048c8d3541df6ab5becff4cd7413d28859
Ajay Yadav <ajju.kvmc@gmail.com>
2015-04-03 04:17:45 +0530

    adding bootstrap template

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,9 @@ from .models import Post
 from .forms import PostForm
 
 # Create your views here.
-def post_list(request):
+def main(request):
 	posts = Post.objects.filter(published_date__isnull=False).order_by('published_date')
-	return render(request, 'blog/post_list.html', {'posts': posts})
+	return render(request, 'blog/index.html', {'posts': posts})
 
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
